bdd4f73e1c38a59b9a06008e9c36085fc1a51055
Alexander Missaoui <alexandermissaoui@gmail.com>
2023-02-03 17:52:33 +0100

    klar

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const listItem = document.createElement("li");
 const submitBtn = document.querySelector("#addtodo");
 const todoInput = document.querySelector("#todoInput")
 const clearText = document.querySelector('.todoInput');
-const errorMessage = document.querySelector("#errorMessage")
+const errorMessage = document.querySelector("#errorMessage");
 const toDoArray= []
 const id = toDoArray.length +1
 let BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
-
 // Fetch get todos
   window.onload = function() {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
@@ -19,7 +18,6 @@ let BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
           toDoArray.push(todo)
         })
         console.log(toDoArray)
-
         // Iterate through the todos and add them to the list
         for (let i = 0; i < todos.length; i++) {
           let todo = todos[i];
@@ -31,19 +29,16 @@ let BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
        }
     });
   }
-  
-// remove todo
+// Remove todo
   function removeTodo(element) {
     const todoId = element.parentNode.getAttribute("data-id");
     fetch(BASE_URL + todoId, {
       method: 'DELETE'
-      
     }).then(response => {
         if (response.ok) {
           console.log(response)
           const index = toDoArray.indexOf(todoId => todoId == index)
           toDoArray.splice(index, 1)
-
             element.parentNode.remove();
         }
         else {
@@ -51,32 +46,26 @@ let BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
         }
     });
   }
-
   // Validation no empty todo
   function addTodo(e) {
     e.preventDefault()
     const todoInput = document.getElementById("todoInput").value;
     if (todoInput.trim() === "") {
       errorMessage.classList.remove('d-none');
-      return
-    // } else {  
+      return 
     }
     errorMessage.classList.add('d-none');
-
   // Send to API
       const newTodo = {
         userId: 11,
         title: todoInput,
         completed: false,
     }
-
   // Add to the list
     let listItem = document.createElement("li");
     listItem.innerText = todoInput;
-    // listItem.setAttribute("data-id", todo.id);
     listItem.innerHTML += ' <button onclick="removeTodo(this)">Remove</button>';
     todoList.appendChild(listItem);
-
   fetch('https://jsonplaceholder.typicode.com/todos', {
     method: 'POST',
     body: JSON.stringify(newTodo),
